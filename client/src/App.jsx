3a7903eb4680b472111ced9253609bcd19767746
Anmol <anmolsharma3843@gmail.com>
2026-07-02 16:04:26 +0530

    Refactor App component to remove token handling
    Removed token state management and related effect.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import Login from "./Login";
 import ChatApp from "./ChatApp";
-import { jwtDecode } from "jwt-decode";
-
 function App() {
   const [user, setUser] = useState(null);
-  const [token, setToken] = useState(null);
-
-  useEffect(() => {
-    const fetchToken = async () => {
-      const cookie = await cookieStore.get("token"); // 👈 await here
-      if (cookie) {
-        setToken(cookie.value);
-        const decoded = jwtDecode(cookie.value);
-        setUser(decoded); // or however you want to set user
-      }
-    };
-    fetchToken();
-  }, []);
 
   return (
     <>
